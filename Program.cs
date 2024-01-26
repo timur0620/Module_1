6a8f53b24646b5e376ac58759ed48b0eae89b05b
Timur <timur0620@list.ru>
2024-01-26 12:13:35 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+namespace Module_1
+{
+    internal class Program
+    {   
+        /// <summary>
+        ///  First class
+        /// </summary>
+        /// <param name="args">Argument</param>
+        static void Main(string[] args)
+        {   
+            WriteLine("Hello, World!");
+            Write("Hello,");
+            Write(" World");              // Home Work Module_1
+            Write("!!!");
+            ReadKey();
+        }
+    }
+}
